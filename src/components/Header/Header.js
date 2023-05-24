41ed200d47879b358988b8bda1051ec867ed439a
Nam Anh Mai <maithenamanh@gmail.com>
2023-05-24 00:46:59 -0700

    Added header for laptop layout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import { Menu, Search, User } from 'react-feather';
 
-import { QUERIES } from '../../constants';
+import { COLORS, QUERIES, WEIGHTS } from '../../constants';
 
 import MaxWidthWrapper from '../MaxWidthWrapper';
 import Logo from '../Logo';
@@ -29,7 +29,23 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <LaptopActionGroup>
+          <button>
+            <Search size={24} />
+          </button>
+          <button>
+            <Menu size={24} />
+          </button>
+        </LaptopActionGroup>
         <Logo />
+        <SubscribeWrapper>
+          <Button>
+            Subscribe
+          </Button>
+          <SubscribeLink href="/">
+            Already a subscriber?
+          </SubscribeLink> 
+        </SubscribeWrapper>
       </MainHeader>
     </header>
   );
@@ -39,6 +55,10 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -59,12 +79,47 @@ const ActionGroup = styled.div`
   }
 `;
 
+const LaptopActionGroup = styled(ActionGroup)`
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: flex;
+  }
+`
 const MainHeader = styled(MaxWidthWrapper)`
   display: flex;
   align-items: center;
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: grid;
+    grid-auto-flow: column;
+    grid-template-columns: 1fr auto 1fr;
+    align-items: center;
+    justify-content: revert;
+  }
 `;
 
+const SubscribeWrapper = styled.div`
+  display: none;
+  
+  @media ${QUERIES.laptopAndUp} {
+    justify-self: end;
+    align-self: end;
+
+    display: grid;
+    gap: 8px;
+    justify-items: center;
+  }
+`
+
+const SubscribeLink = styled.a`
+  font-size: 0.875rem;
+  color: ${COLORS.gray[400]};
+  text-decoration: underline;
+  font-style: italic;
+  font-weight: ${WEIGHTS.normal};
+`
 export default Header;
